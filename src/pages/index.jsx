a76762ff1f2f1e1e1e20d46f5daf6145664945e6
Carmen1104 <carmencongko11@gmail.com>
2023-05-22 10:47:53 +0700

    upload

---
+++ src/pages/index.jsx
@@ -0,0 +1,5 @@
+export { default as Assignment } from './Assignment';
+export { default as Classes } from './Classes';
+export { default as Dashboard } from './Dashboard';
+export { default as Schedule } from './Schedule';
+
